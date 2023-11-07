15fa7a6f312636f48630170631909c2550f20a41
Elina Stepanova <149301527+elistep@users.noreply.github.com>
2023-11-07 23:23:35 +0500

    Add files via upload

---
+++ task2/task2.py
@@ -0,0 +1,28 @@
+import numpy as np
+import os
+
+matrix = np.load("matrix_57_2.npy")
+
+size = len(matrix)
+
+x = list()
+y = list()
+z = list()
+
+limit = 557
+
+for i in range(0, size):
+    for j in range(0, size):
+        if matrix[i][j] > limit:
+            x.append(i)
+            y.append(j)
+            z.append(matrix[i][j])
+
+np.savez("points", x=x, y=y, z=z)
+np.savez_compressed("points_zip", x=x, y=y, z=z)
+
+print(f"points = {os.path.getsize('points.npz')}")
+print(f"points_zip = {os.path.getsize('points_zip.npz')}")
+
+# plot_data = np.load("points.npz")
+# plot_data_compress = np.load("points_zip.npz")
