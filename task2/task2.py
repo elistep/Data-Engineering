bc4b5f3307d33f874f65579e268b55329acf54a9
Elina Stepanova <149301527+elistep@users.noreply.github.com>
2023-12-06 01:01:48 +0500

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -49,9 +49,6 @@ for phone in items:
     if phone['price'] < 10050:
         filtered_items.append(phone)
 
-with open("result_filtered_2.json", "w", encoding="utf-8") as f:
-   f.write(json.dumps(filtered_items, ensure_ascii=False))
-
 #print(len(items))
 #print(len(filtered_items))
 
@@ -63,7 +60,7 @@ pd.set_option('display.float_format', '{:.1f}'.format)
 stats = df['price'].agg(['max', 'min', 'mean', 'median', 'std']).to_dict()
 result.append(stats)
 
-print(result)
+#print(result)
 
 result2 = []
 
@@ -71,4 +68,13 @@ words = [item['title'] for item in items]
 f1 = collections.Counter(words)
 result2.append(f1)
 
-print(result2)
+#print(result2)
+
+with open("result_filtered_2.json", "w", encoding="utf-8") as f:
+   f.write(json.dumps(filtered_items, ensure_ascii=False))
+
+with open("math_2.json", "w", encoding="utf-8") as f:
+   f.write(json.dumps(result, ensure_ascii=False))
+
+with open("frequency_2.json", "w", encoding="utf-8") as f:
+   f.write(json.dumps(result2, ensure_ascii=False))
